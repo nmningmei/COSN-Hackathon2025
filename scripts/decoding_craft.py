74deedebf139615158135db6cf3d29764f7d5ce8
宋奕德 <645548158@qq.com>
2025-08-24 12:10:18 +0800

    加了一点注释，把maga_for_loop的函数提取出来了，把epoch传进去，输出mag和grad及其领域的idx的list

--- scripts/decoding_craft.py
+++ scripts/decoding_craft.py
@@ -32,29 +32,31 @@ from joblib import Parallel, delayed
 
 from typing import Any
 
-def get_neighbors_within_radius(pos, radius:float,) -> list[int | list[int] | list[list[int]] | list[list[list[Any]]]]:
-    
+
+def get_neighbors_within_radius(pos, radius: float, ) -> list[
+    int | list[int] | list[list[int]] | list[list[list[Any]]]]:
     dist_mat = cdist(pos, pos)
     neighbors = [np.where(dist_mat[i] <= radius)[0].tolist() for i in range(len(pos))]
     return neighbors
 
-def get_chanpo_fif(subj:int,radius:float):
-    datadir = r'../data'
-    sub_fif_dir = r'cleaned/unconscious-session_1-block_1-epo.fif'
-    epo_fif_dir = os.path.join(datadir,'MEG',f'subject{subj}',sub_fif_dir)
-    epo_fif = mne.read_epochs(epo_fif_dir, preload=False)
+
+def get_chanpo_fif(example_epochs, radius: float):
+    # datadir = r'../data'
+    # sub_fif_dir = r'cleaned/unconscious-session_1-block_1-epo.fif'
+    # epo_fif_dir = os.path.join(datadir, 'MEG', f'subject{subj}', sub_fif_dir)
+    # example_epochs = mne.read_epochs(example_epochs_dir, preload=False)
 
     # 提取磁强计索引
-    mag_idx = mne.pick_types(epo_fif.info, meg='mag')
+    mag_idx = mne.pick_types(example_epochs.info, meg='mag')
 
     # 提取梯度计索引
-    grad_idx = mne.pick_types(epo_fif.info, meg='grad')
+    grad_idx = mne.pick_types(example_epochs.info, meg='grad')
 
     # 获取磁强计的位置
-    mag_pos = [epo_fif.info['chs'][i]['loc'][:3] for i in mag_idx]
+    mag_pos = [example_epochs.info['chs'][i]['loc'][:3] for i in mag_idx]
 
     # 获取梯度计的位置
-    grad_pos_l = [epo_fif.info['chs'][i]['loc'][:3] for i in grad_idx]
+    grad_pos_l = [example_epochs.info['chs'][i]['loc'][:3] for i in grad_idx]
     grad_pos = np.array(grad_pos_l)
     # grad_pos_102 = grad_pos.reshape(-1,2,3)
     # grad_pos_c = np.mean(grad_pos_102,axis = 1)
@@ -62,7 +64,7 @@ def get_chanpo_fif(subj:int,radius:float):
     #
     mag_idx_idx = get_neighbors_within_radius(mag_pos, radius)
     #
-    grad_idx_idx = np.arange(len(grad_pos)).reshape(-1,2)
+    grad_idx_idx = np.arange(len(grad_pos)).reshape(-1, 2)
     grad_idx_idx = [grad_idx_idx[item].flatten() for item in mag_idx_idx]
 
     return mag_idx_idx, grad_idx_idx
@@ -70,10 +72,9 @@ def get_chanpo_fif(subj:int,radius:float):
 
 def str2int(x):
     if type(x) is str:
-        return int(re.findall(r'\d+',x)[0])
+        return int(re.findall(r'\d+', x)[0])
     else:
         return x
-
 
 
 def build_model_dictionary(model_name: str = 'None + Linear-SVM',
@@ -190,11 +191,38 @@ def build_model_dictionary(model_name: str = 'None + Linear-SVM',
 
     return make_pipeline(*pipeline)
 
+def get_neighbors_idx(epochs, radius:float):
+    mag_idx_idx, grad_idx_idx = get_chanpo_fif(epochs, radius)
+    time = np.arange(epochs.times.shape[0])
+    maga_for_loop_mag = list(itertools.product(mag_idx_idx, time))
+    maga_for_loop_grad = list(itertools.product(grad_idx_idx, time))
+    return maga_for_loop_mag, maga_for_loop_grad
 
+def load_data(subj: int, radius: float = .04):
+    """
+    Load MEG data and behavioral data for a given subject.
 
+    Parameters
+    ----------
+    subj : int
+        Subject number to load the data for.
+    radius : float, optional
+        Radius for neighborhood search in the MEG data. Default is 0.04.
 
-def load_data(subj:int,radius:float = .04,):
-    subject = 'subject'+str(subj)
+    Returns
+    -------
+    maga_for_loop : list
+        List of tuples containing magnetic sensor indices and time points for decoding.
+    epochs_unconscious : mne.Epochs
+        MEG epochs data for unconscious trials.
+    epochs_conscious : mne.Epochs
+        MEG epochs data for conscious trials.
+    df_unconscious : pandas.DataFrame
+        Behavioral data for unconscious trials.
+    df_conscious : pandas.DataFrame
+        Behavioral data for conscious trials.
+    """
+    subject = 'subject' + str(subj)
     working_dir = os.path.join('../data/MEG', subject)
     working_data = np.sort(glob(os.path.join(working_dir, "cleaned", "unconscious-session*fif")))
     working_beha = np.sort(glob(os.path.join(working_dir, "cleaned", "unconscious-session*csv")))
@@ -208,7 +236,7 @@ def load_data(subj:int,radius:float = .04,):
     epochs = mne.concatenate_epochs(epochs)
     #     epochs.resample(100)
     df = pd.concat(df)
-    df["visible.keys_raw"]=df["visible.keys_raw"].apply(str2int)
+    df["visible.keys_raw"] = df["visible.keys_raw"].apply(str2int)
     idx_unconscious = df['visible.keys_raw'] == 1
     idx_conscious = df['visible.keys_raw'] == 3
     epochs_unconscious = epochs[idx_unconscious]
@@ -220,8 +248,9 @@ def load_data(subj:int,radius:float = .04,):
     time = np.arange(epochs.times.shape[0])
     maga_for_loop = list(itertools.product(mag_idx_idx, time))
     del epochs
-    print('1')
     return maga_for_loop, epochs_unconscious, epochs_conscious, df_unconscious, df_conscious
+
+
 #     if mag & grad:
 #
 #         mag_data=epochs.copy().pick(picks='mag').pick(mag_idx_idx[0])
@@ -232,28 +261,54 @@ def load_data(subj:int,radius:float = .04,):
 #
 #     mag_data=epochs.copy().pick(picks='mag').pick(mag_idx_idx[0])
 #
-label_map = {"Nonliving_Things":1,
-             "Living_Things":0,
+label_map = {"Nonliving_Things": 1,
+             "Living_Things": 0,
              }
-def decode_within_sphere(epoch_data,labels,cv,model_name='None + Linear-SVM'):
+
+
+def decode_within_sphere(epoch_data, labels, cv, model_name='None + Linear-SVM'):
+    """
+        Perform decoding within a sphere using a specified machine learning model.
+
+        Parameters
+        ----------
+        epoch_data : ndarray
+            The MEG data to be used for decoding. Shape is typically (n_samples, n_features).
+        labels : ndarray
+            The labels corresponding to the data samples.
+        cv : sklearn.model_selection._BaseKFold
+            Cross-validation splitting strategy.
+        model_name : str, optional
+            The name of the model to use for decoding. Default is 'None + Linear-SVM'.
+            The format is 'FeatureExtractor + Decoder', where the feature extractor and decoder
+            are specified as strings.
+
+        Returns
+        -------
+        test_scores : ndarray
+            The cross-validated test scores for the decoding task.
+        """
     pipeline = build_model_dictionary(model_name=model_name)
-    res = cross_validate(pipeline,epoch_data,labels,cv = cv,scoring = 'roc_auc',n_jobs = 1)
+    res = cross_validate(pipeline, epoch_data, labels, cv=cv, scoring='roc_auc', n_jobs=1)
     return res['test_score']
+
+
 
 if __name__ == '__main__':
     subj = 9
     subject = 'subject' + str(subj)
-    maga_for_loop, epochs_unconscious, epochs_conscious, df_unconscious, df_conscious=load_data(9,.04,True,True)
+    maga_for_loop, epochs_unconscious, epochs_conscious, df_unconscious, df_conscious = load_data(9, .04, True, True)
     labels_unconscious = df_unconscious['category'].map(label_map).values
     labels_conscious = df_conscious['category'].map(label_map).values
 
     cv = StratifiedShuffleSplit(n_splits=20, test_size=0.2, random_state=12345)
 
-    scores = Parallel(n_jobs = 2,verbose = 1,)(delayed(decode_within_sphere)(**{'epoch_data':epochs_unconscious.get_data()[:,sphere,time_point],
-                                                                                'labels':labels_unconscious,
-                                                                                'cv': cv, }) for sphere, time_point in maga_for_loop)
-    results_dir = os.path.join('../results/','MEG',subject)
+    scores = Parallel(n_jobs=2, verbose=1, )(
+        delayed(decode_within_sphere)(**{'epoch_data': epochs_unconscious.get_data()[:, sphere, time_point],
+                                         'labels': labels_unconscious,
+                                         'cv': cv, }) for sphere, time_point in maga_for_loop)
+    results_dir = os.path.join('../results/', 'MEG', subject)
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
-    np.save(os.path.join(results_dir,'searchlight_scores.npy'),scores)
+    np.save(os.path.join(results_dir, 'searchlight_scores.npy'), scores)
 
